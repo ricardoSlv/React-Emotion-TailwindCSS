90be4aef414cef0750869b9aadccc99ef962aa90
ricardoslv <ricardoslv80@gmail.com>
2020-10-08 22:09:12 +0100

    Add a more complete css emotion class with tw inside

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
+import { jsx, css } from '@emotion/core'
+import styled from '@emotion/styled'
 import tw from 'twin.macro';
 
 function App() {
@@ -23,6 +24,31 @@ function App() {
 function Card() {
   const Wrapper = tw.div`max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl`
 
+  const complexSelector = css({
+    background: ['red', 'linear-gradient(#e66465, #9198e5)'],
+    '&>*': tw`text-gray-300 font-bold py-2`,
+    '&>p:nth-of-type(3)': css({ backgroundColor: 'green' }),
+    '*:first-of-type':tw`bg-red-700`,
+    '&>p': css({ backgroundColor: 'blue' }, tw`pl-4`),
+    
+    // '&:nth-child(2)': tw`bg-red-700`,
+    // '&:first-child': tw`bg-red-100`,
+    //Console erros on child selectore warning they unsafe for SSR, 
+    //Docs say they can be worked around but encourage  nth-of-type(), first-of-type() instead
+  }
+    , tw`text-center rounded  overflow-hidden mb-2`
+  )
+
+  const ComplexSelectorDiv = styled.div({
+    background: ['red', 'linear-gradient(#e66465, #9198e5)'],
+    '*:first-of-type':tw`bg-red-700`,
+    '>*': tw`text-gray-300 font-bold py-2`,
+    '>p:nth-of-type(3)': css({ backgroundColor: 'green' }),
+    '>p': css({ backgroundColor: 'blue' }, tw`pl-4`),
+  }
+    , tw`text-center rounded overflow-hidden `
+  )
+
   return (
     <Wrapper>
       <div tw="flex-shrink-0">
@@ -31,6 +57,18 @@ function Card() {
       <div tw="ml-2 pt-1" >
         <h4 tw="text-xl text-gray-900 leading-tight" >ChitChat</h4>
         <p tw="text-base text-gray-600 leading-normal" >You have a new message!</p>
+        <div css={complexSelector}>
+          <p>Hey1</p>
+          <p>Hey2</p>
+          <p>Hey3</p>
+          <p>Hey4</p>
+        </div>
+        <ComplexSelectorDiv>
+          <p>Hey1</p>
+          <p>Hey2</p>
+          <p>Hey3</p>
+          <p>Hey4</p>
+        </ComplexSelectorDiv>
       </div>
     </Wrapper>
   )
